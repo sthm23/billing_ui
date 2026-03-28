9c889fefc0234a8d97d777e446618794b16c9bbe
Sanjar Tukhtamishev <stuxtamixhev@gmail.com>
2026-03-28 06:39:03 +0500

    feat(i18n): update sidebar translations for English, Russian, and Uzbek; enhance menu component with dynamic translation support

--- src/app/layout/components/menu/menu.ts
+++ src/app/layout/components/menu/menu.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
 import { MenuItems } from '../menu-item/menu-item';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MenuItem } from 'primeng/api';
 import { AuthService } from '../../../pages/auth/service/auth';
-
+import { TranslocoService } from '@ngneat/transloco';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 @Component({
   selector: 'app-menu',
   imports: [CommonModule, MenuItems, RouterModule],
@@ -13,120 +14,127 @@ import { AuthService } from '../../../pages/auth/service/auth';
 })
 export class Menu {
   model: MenuItem[] = [];
+  private destroyRef = inject(DestroyRef);
 
   constructor(
-    private authService: AuthService
+    private authService: AuthService,
+    private translate: TranslocoService
   ) { }
 
   ngOnInit() {
-    this.model = [
-      {
-        label: 'Home',
-        items: [{ label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/'] }]
-      },
-      {
-        label: 'Pages',
-        icon: 'pi pi-fw pi-briefcase',
-        routerLink: ['/pages'],
-        items: [
-          // {
-          //   label: 'Landing',
-          //   icon: 'pi pi-fw pi-globe',
-          //   routerLink: ['/landing']
-          // },
-          {
-            label: 'Organization',
-            icon: 'pi pi-fw pi-sitemap',
-            visible: this.authService.isAdmin(),
-            items: [
-              {
-                label: 'Store List',
-                icon: 'pi pi-fw pi-globe',
-                routerLink: ['/pages/organization/list']
-              },
-              {
-                label: 'Create Store',
-                icon: 'pi pi-fw pi-globe',
-                routerLink: ['/pages/organization/create']
-              },
-            ]
-          },
-          {
-            label: 'Product',
-            icon: 'pi pi-fw pi-tag',
-            items: [
-              {
-                label: 'List',
-                icon: 'pi pi-fw pi-list',
-                routerLink: ['/pages/product/list']
-              },
-              {
-                label: 'Create',
-                icon: 'pi pi-fw pi-plus-circle',
-                routerLink: ['/pages/product/create']
-              }
-            ]
-          },
-          {
-            label: 'User',
-            icon: 'pi pi-fw pi-users',
-            items: [
-              {
-                label: 'List',
-                icon: 'pi pi-fw pi-list',
-                routerLink: ['/pages/user/list']
-              },
-              {
-                label: 'Create',
-                icon: 'pi pi-fw pi-plus-circle',
-                routerLink: ['/pages/user/create']
-              }
-            ]
-          },
-          {
-            label: 'Order',
-            icon: 'pi pi-fw pi-shopping-cart',
-            items: [
-              {
-                label: 'List',
-                icon: 'pi pi-fw pi-list',
-                routerLink: ['/pages/order/list']
-              }
-            ]
-          },
-        ]
-      },
 
-      {
-        label: 'Settings',
-        items: [
-          {
-            label: 'Profile',
-            icon: 'pi pi-fw pi-cog',
-            routerLink: ['/settings/profile'],
-          },
+    this.translate.selectTranslateObject('sidebar').pipe(
+      takeUntilDestroyed(this.destroyRef)
+    ).subscribe((res: Record<string, string>) => {
+      this.model = [
+        {
+          label: res['home'],
+          items: [{ label: res['dashboard'], icon: 'pi pi-fw pi-home', routerLink: ['/'] }]
+        },
+        {
+          label: res['pages'],
+          icon: 'pi pi-fw pi-briefcase',
+          routerLink: ['/pages'],
+          items: [
+            // {
+            //   label: 'Landing',
+            //   icon: 'pi pi-fw pi-globe',
+            //   routerLink: ['/landing']
+            // },
+            {
+              label: res['organization'],
+              icon: 'pi pi-fw pi-sitemap',
+              visible: this.authService.isAdmin(),
+              items: [
+                {
+                  label: res['storeList'],
+                  icon: 'pi pi-fw pi-globe',
+                  routerLink: ['/pages/organization/list']
+                },
+                {
+                  label: res['createStore'],
+                  icon: 'pi pi-fw pi-globe',
+                  routerLink: ['/pages/organization/create']
+                },
+              ]
+            },
+            {
+              label: res['product'],
+              icon: 'pi pi-fw pi-tag',
+              items: [
+                {
+                  label: res['list'],
+                  icon: 'pi pi-fw pi-list',
+                  routerLink: ['/pages/product/list']
+                },
+                {
+                  label: res['create'],
+                  icon: 'pi pi-fw pi-plus-circle',
+                  routerLink: ['/pages/product/create']
+                }
+              ]
+            },
+            {
+              label: res['user'],
+              icon: 'pi pi-fw pi-users',
+              items: [
+                {
+                  label: res['list'],
+                  icon: 'pi pi-fw pi-list',
+                  routerLink: ['/pages/user/list']
+                },
+                {
+                  label: res['create'],
+                  icon: 'pi pi-fw pi-plus-circle',
+                  routerLink: ['/pages/user/create']
+                }
+              ]
+            },
+            {
+              label: res['order'],
+              icon: 'pi pi-fw pi-shopping-cart',
+              items: [
+                {
+                  label: res['list'],
+                  icon: 'pi pi-fw pi-list',
+                  routerLink: ['/pages/order/list']
+                }
+              ]
+            },
+          ]
+        },
 
-          {
-            label: 'Attribute',
-            icon: 'pi pi-fw pi-cog',
-            routerLink: ['/settings/attribute/list'],
-            items: [
-              {
-                label: 'List',
-                icon: 'pi pi-fw pi-list',
-                routerLink: ['/settings/attribute/list']
-              },
-              {
-                label: 'Create',
-                icon: 'pi pi-fw pi-plus-circle',
-                routerLink: ['/settings/attribute/create']
-              }
-            ]
-          },
+        {
+          label: res['settings'],
+          items: [
+            {
+              label: res['profile'],
+              icon: 'pi pi-fw pi-cog',
+              routerLink: ['/settings/profile'],
+            },
+
+            {
+              label: res['attribute'],
+              icon: 'pi pi-fw pi-cog',
+              routerLink: ['/settings/attribute/list'],
+              items: [
+                {
+                  label: res['list'],
+                  icon: 'pi pi-fw pi-list',
+                  routerLink: ['/settings/attribute/list']
+                },
+                {
+                  label: res['create'],
+                  icon: 'pi pi-fw pi-plus-circle',
+                  routerLink: ['/settings/attribute/create']
+                }
+              ]
+            },
+          ]
+        }
+      ];
+    });
 
 
-        ]
-      }
-    ];
   }
 }
