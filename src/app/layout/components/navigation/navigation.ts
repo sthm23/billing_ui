0c431bbf2178bc97ab179994d2eb225fba3e60e3
Sanjar Tukhtamishev <stuxtamixhev@gmail.com>
2026-05-16 20:07:17 +0500

    feat(navigation): enhance navigation component with localized labels and routing functionality

--- src/app/layout/components/navigation/navigation.ts
+++ src/app/layout/components/navigation/navigation.ts
@@ -2,11 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { SpeedDialModule } from 'primeng/speeddial';
+import { AuthService } from '../../../pages/auth/service/auth';
+import { TranslocoPipe } from '@ngneat/transloco';
+import { Router } from '@angular/router';
+import { TranslateService } from '../../../shared/services/translate.service';
 
 @Component({
   selector: 'app-navigation',
   imports: [
     ButtonModule, SpeedDialModule,
+    TranslocoPipe
   ],
   templateUrl: './navigation.html',
   styleUrl: './navigation.css',
@@ -14,32 +19,81 @@ import { SpeedDialModule } from 'primeng/speeddial';
 export class Navigation implements OnInit {
   items: MenuItem[] = [];
 
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private translate: TranslateService
+  ) { }
+
   ngOnInit() {
-    this.items = [
-      {
-        label: 'Add',
-        icon: 'pi pi-pencil',
-      },
-      {
-        label: 'Update',
-        icon: 'pi pi-refresh',
-      },
-      {
-        label: 'Delete',
-        icon: 'pi pi-trash',
-      },
-      {
-        label: 'Upload',
-        icon: 'pi pi-upload',
-      },
-      {
-        label: 'Website',
-        icon: 'pi pi-external-link',
-      }
-    ];
+    this.translate.selectTranslateObject('sidebar').subscribe((res) => {
+      this.items = [
+        {
+          label: res['dashboard'],
+          icon: 'pi pi-fw pi-home',
+          visible: this.authService.isAdmin() || this.authService.isOwner(),
+          command: () => this.goTo('dashboard')
+        },
+        {
+          label: res['profile'],
+          icon: 'pi pi-fw pi-user',
+          command: () => this.goTo('profile')
+        },
+        {
+          label: res['organization'],
+          icon: 'pi pi-fw pi-sitemap',
+          visible: this.authService.isAdmin(),
+          command: () => this.goTo('organization')
+        },
+        {
+          label: res['user'],
+          icon: 'pi pi-fw pi-users',
+          visible: this.authService.isOwner() || this.authService.isAdmin(),
+          command: () => this.goTo('user')
+        },
+        {
+          label: res['attribute'],
+          icon: 'pi pi-fw pi-tag',
+          visible: this.authService.isAdmin(),
+          command: () => this.goTo('attribute')
+        }
+      ];
+    });
   }
 
-  toggleCallback(event: any, item?: MenuItem) {
-    console.log(event, item);
+  goTo(key: string) {
+    switch (key) {
+      case 'product':
+        this.router.navigate(['/pages/product/list'])
+        break;
+      case 'payment':
+        this.router.navigate(['/pages/payments/list'])
+        break;
+      case 'order':
+        this.router.navigate(['/pages/order/list'])
+        break;
+      case 'debitor':
+        this.router.navigate(['/pages/debitor/list'])
+        break;
+      case 'dashboard':
+        this.router.navigate([''])
+        break;
+      case 'user':
+        this.router.navigate(['/pages/user/list'])
+        break;
+      case 'profile':
+        this.router.navigate(['/pages/profile'])
+        break;
+
+      case 'attribute':
+        this.router.navigate(['/pages/settings/attribute/list'])
+        break;
+      case 'organization':
+        this.router.navigate(['/pages/organization/list'])
+        break;
+
+      default:
+        break;
+    }
   }
 }
